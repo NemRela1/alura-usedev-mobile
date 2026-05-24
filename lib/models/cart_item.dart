1c54c215052e0e6385d5dfbe5b222233de96fa88
Lucas Magalhães <conceicaolucas68@gmail.com>
2026-05-24 06:33:55 -0300

    Criação dos models e services fakestoreapi

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,13 @@
+import 'package:app/models/product.dart';
+
+class CartItem {
+  final ProductModel product;
+  int quantity;
+
+  CartItem({
+    required this.product,
+    this.quantity = 1,
+  });
+
+  double get totalPrice => product.price * quantity;
+}
